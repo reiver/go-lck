0e30233d682d869e2dedf725aaf470811473991d
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-08-05 12:32:48 -0700

    lockable

--- lockable.go
+++ lockable.go
@@ -4,19 +4,19 @@ import (
 	"sync"
 )
 
-type Locking[T any] struct {
+type Lockable[T any] struct {
 	value T
 	mutex sync.RWMutex
 }
 
-func (receiver *Locking[T]) Get() T {
+func (receiver *Lockable[T]) Get() T {
 	receiver.mutex.RLock()
 	defer receiver.mutex.RUnlock()
 
 	return receiver.value
 }
 
-func (receiver *Locking[T]) Set(value T) {
+func (receiver *Lockable[T]) Set(value T) {
 	receiver.mutex.Lock()
 	defer receiver.mutex.Unlock()
 

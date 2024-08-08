4306a52b75ad8178fc1a6322c5bbce537f5c7d7b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-08-08 06:25:29 -0700

    let

--- lockable.go
+++ lockable.go
@@ -16,6 +16,12 @@ func (receiver *Lockable[T]) Get() T {
 	return receiver.value
 }
 
+func (receiver *Lockable[T]) Let(fn func(*T)) {
+	receiver.mutex.RLock()
+	defer receiver.mutex.RUnlock()
+
+	fn(&receiver.value)
+}
 func (receiver *Lockable[T]) Set(value T) {
 	receiver.mutex.Lock()
 	defer receiver.mutex.Unlock()

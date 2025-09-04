e5c3d05597b613a8ba76134cf4174adad1638961
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-09-04 08:20:24 -0700

    lck.Map

---
+++ map.go
@@ -0,0 +1,84 @@
+package lck
+
+import (
+	"cmp"
+	"slices"
+	"sync"
+)
+
+type Map[K cmp.Ordered, V any] struct {
+	mutex sync.Mutex
+	data map[K]V
+}
+
+func (receiver *Map[K,V]) For(fn func(K,V)) {
+	if nil == receiver {
+		return
+	}
+
+	receiver.mutex.Lock()
+	defer receiver.mutex.Unlock()
+
+	var keys []K
+	for key,_ := range receiver.data {
+		keys = append(keys, key)
+	}
+
+	slices.Sort(keys)
+
+	for _, key := range keys {
+		value := receiver.data[key]
+		fn(key,value)
+	}
+}
+
+func (receiver *Map[K,V]) Get(key K) (V, bool) {
+	var nada V
+
+	if nil == receiver {
+		return nada, false
+	}
+
+	receiver.mutex.Lock()
+	defer receiver.mutex.Unlock()
+
+	if len(receiver.data) <= 0 {
+		return nada, false
+	}
+
+	value, found := receiver.data[key]
+	if !found {
+		return nada, false
+	}
+
+	return value, true
+}
+
+func (receiver *Map[K,V]) Len() int {
+	if nil == receiver {
+		return 0
+	}
+
+	receiver.mutex.Lock()
+	defer receiver.mutex.Unlock()
+
+	return len(receiver.data)
+}
+
+func (receiver *Map[K,V]) Set(key K, value V) {
+	if nil == receiver {
+		return
+	}
+
+	receiver.mutex.Lock()
+	defer receiver.mutex.Unlock()
+
+	if nil == receiver.data {
+		receiver.data = map[K]V{}
+	}
+	if nil == receiver.data {
+		return
+	}
+
+	receiver.data[key] = value
+}
---
+++ map_examples_test.go
@@ -0,0 +1,48 @@
+package lck_test
+
+import (
+	"fmt"
+
+	"github.com/reiver/go-lck"
+)
+
+func ExampleMap_string_int() {
+
+	var store lck.Map[string,int]
+
+	fmt.Printf("INITIAL-STORE-LEN: %d\n", store.Len())
+
+	store.Set("once", 1)
+	store.Set("twice", 2)
+	store.Set("thrice", 3)
+	store.Set("fource", 4)
+
+	fmt.Printf("STORE-LEN-AFTER-SETTING: %d\n", store.Len())
+
+	value, found := store.Get("twice")
+	fmt.Printf("STORE[twice]: %d, %t\n", value, found)
+
+	store.For(func(key string, value int){
+		fmt.Println()
+		fmt.Printf("KEY: %q\n", key)
+		fmt.Printf("VALUE: %d\n", value)
+	})
+
+	// Output:
+	// INITIAL-STORE-LEN: 0
+	// STORE-LEN-AFTER-SETTING: 4
+	// STORE[twice]: 2, true
+	// 
+	// KEY: "fource"
+	// VALUE: 4
+	// 
+	// KEY: "once"
+	// VALUE: 1
+	//
+	// KEY: "thrice"
+	// VALUE: 3
+	// 
+	// KEY: "twice"
+	// VALUE: 2
+
+}
